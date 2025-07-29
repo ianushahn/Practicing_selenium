1152ba24d6f035669194419b1b071e5664244400
ianushahn <poornimapushpa5@gmail.com>
2025-07-29 16:04:23 +0530

    merging the codes

---
+++ selenium/src/selenium/WebDriver_Abstraction_methods.java
@@ -0,0 +1,12 @@
+package selenium;
+
+public class WebDriver_Abstraction_methods
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("WebDriver Abstraction methods");
+
+	}
+
+}
